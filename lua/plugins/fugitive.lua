035816ec93b37c94dd2b80ea1315617f615d3492
randomctl <randomctl@proton.me>
2026-06-01 01:24:31 +0100

    Format fugitive.lua

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -7,18 +7,18 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
             return
         end
         local bufnr = vim.api.nvim_get_current_buf()
-        local opts = {buffer = bufnr, remap = false}
+        local opts = { buffer = bufnr, remap = false }
         vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git('push')
-                end, opts)
+            vim.cmd.Git('push')
+        end, opts)
 
-                -- rebase always
-                vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
-                end, opts)
+        -- rebase always
+        vim.keymap.set("n", "<leader>P", function()
+            vim.cmd.Git({ 'pull', '--rebase' })
+        end, opts)
 
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
     end,
-}) 
+})
 vim.keymap.set("n", "<leader>df", ":Gdiffsplit<CR>")
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
